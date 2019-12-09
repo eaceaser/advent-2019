30df778a91c144b1b93ae8c6ef8d5862048a2514
Ed Ceaser <ceasere@gmail.com>
2019-12-09 14:33:34 -0500

    day 9 part 1

---
+++ 9/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+const (
+	MemSize = 64 * 1024
+)
+
+func main() {
+	memS, err := ioutil.ReadFile("input")
+	if err != nil {
+		panic(err)
+	}
+
+	memS1 := strings.Split(strings.TrimSpace(string(memS)), ",")
+	mem := make([]int, MemSize)
+	for i, s := range memS1 {
+		mem[i], err = strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	input := make(chan int, 1)
+	input <- 1
+	output := make(chan int)
+	c := intcodeComputer{
+		mem:    mem,
+		input:  input,
+		output: output,
+	}
+
+	wait := sync.WaitGroup{}
+	wait.Add(1)
+	go func() {
+		for {
+			select {
+			case o, ok := <-output:
+				if !ok {
+					wait.Done()
+					return
+				}
+				fmt.Printf("[OUT] %d\n", o)
+			}
+		}
+	}()
+
+	if err := c.Run(); err != nil {
+		panic(err)
+	}
+	close(output)
+	wait.Wait()
+}
